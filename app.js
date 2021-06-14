2e33a8e5a682c713d59993a61fdfacad1eeb755a
StanGitts <85897806+StanGitts@users.noreply.github.com>
2021-06-14 14:47:46 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// C library API
+//const ffi = require('ffi-napi');
+
+// Express App (Routes)
+const express = require("express");
+const app     = express();
+const path    = require("path");
+const fileUpload = require('express-fileupload');
+const mysql = require('mysql2/promise');
+console.log(__dirname);
+// const cp = require('cookie-parser');
+// app.use(cp());
+
+// app.get('/set', (req, res) => {
+//   //set the new style cookie
+//   res.cookie()
+// });
+
+app.use(fileUpload());
+//app.use(express.static(path.join(__dirname+'/uploads')));
+
+// Minimization
+const fs = require('fs');
+const JavaScriptObfuscator = require('javascript-obfuscator');
+
+// Important, pass in port as in `npm run dev 1234`, do not change
+const portNum = process.argv[2];
+
+// Send HTML at root, do not change
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+// Send Style, do not change
+app.get('/style.css',function(req,res){
+  //Feel free to change the contents of style.css to prettify your Web app
+  res.sendFile(path.join(__dirname+'/public/style.css'));
+});
+
+// Send obfuscated JS, do not change
+app.get('/index.js',function(req,res){
+  fs.readFile(path.join(__dirname+'/public/index.js'), 'utf8', function(err, contents) {
+    const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {compact: true, controlFlowFlattening: true});
+    res.contentType('application/javascript');
+    res.send(minimizedContents._obfuscatedCode);
+  });
+});
+
+//sending the image of the logo
+app.get('/logo.jpg', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/assets/new.jpg'));
+})
+
+
+//******************** Your code goes here ******************** 
+
+/**********Setting up the functions**************/
+
+
+
+app.listen(portNum);
+console.log('Running app at localhost: ' + portNum);
+
---
+++ public/index.js
@@ -0,0 +1,38 @@
+document.cookie = "AC-C=ac-c; SameSite=Lax; Secure; Max-Age=2600000";
+// Put all onload AJAX calls here, and event listeners
+jQuery(document).ready(function() {
+    
+    //alert(document.cookie);
+
+    //console.log(document.cookie);
+    // On page-load AJAX Example
+    console.log("Page is ready!")
+
+
+    function removeComponent(parent){
+
+        while(parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    function removeRows(parent){
+        
+        var length = parent.rows.length;
+        //console.log("rows = " + length);
+
+        while(parent.rows.length >= 1){
+            parent.deleteRow(0);
+        }
+            
+    }
+
+    function removeFirstEmpty(id){
+        var parent = document.getElementById(id);
+        parent.deleteRow(0);
+    }
+    
+    
+
+
+});
