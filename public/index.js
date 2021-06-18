6e832dcf94b23d123c9159365be3e0086a320b1b
Zadyn Belgrave <zadynbelgrave@gmail.com>
2021-06-18 11:35:55 -0400

    updated UI when adjusting size of window

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ document.cookie = "AC-C=ac-c; SameSite=Lax; Secure; Max-Age=2600000";
 jQuery(document).ready(function() {
     
     //alert(document.cookie);
+    var wasNavClicked = 0;
 
     //console.log(document.cookie);
     // On page-load AJAX Example
@@ -32,7 +33,36 @@ jQuery(document).ready(function() {
     }
     
     
-    
+    $('#navbar-btn').click(function(e){
+        if (wasNavClicked == 0) {
+            wasNavClicked = 1;
+            console.log("here");
+            e.preventDefault();
+
+            var div = document.getElementById('btn-div');
+            var nav = document.getElementById('nav');
+
+            nav.setAttribute("class", "navbar navbar-extend-md navbar-light navExtend");
+            nav.style.height = "260px";
+            div.setAttribute("class", "navbar-nav divExtend");
+            div.style.height = "200px";
+        }
+        else {
+            wasNavClicked = 0;
+            console.log("off");
+            e.preventDefault();
+
+            var div = document.getElementById('btn-div');
+            var nav = document.getElementById('nav');
+
+            nav.setAttribute("class", "navbar navbar-extend-md navbar-light");
+            nav.removeAttribute("style");
+            div.setAttribute("class", "navbar-nav");
+            div.removeAttribute("style");
+        }
+        
+
+    })
 
     var okFlag = 1;
 
